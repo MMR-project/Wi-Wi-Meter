0fed028a3ec09bf915cd83d2fbba845779c9edd9
ugkasuya <kasuya-u@yama.info.waseda.ac.jp>
2014-04-29 13:28:33 +0900

    debug app_result

--- js/app_result.js
+++ js/app_result.js
@@ -1,5 +1,8 @@
 requirejs.config({
-	baseUrl: 'js',
+    baseUrl: 'js',
+    paths: {
+        lib: './lib'
+    },
 	shim: {
 		"lib/backbone": {
 			deps: ["lib/underscore", "lib/jquery"],
@@ -14,5 +17,63 @@ requirejs.config({
 	}
 });
 
-require([], function(){
+require(["lib/radar", "storage", "history", "history/item"], function(html5jp, Storage, History, HistoryItem){
+	html5jp = html5jp || window.html5jp;
+	var score = Storage.get("score");
+	var answers = Storage.get("answer");
+	var history = Storage.get("history") || new History();
+	var average = answers.average();
+
+
+	var showSubjectiveEvaluation = function(){
+		var rc = new html5jp.graph.radar("sample");
+		if(rc != null) {
+			var items = average.slice(0, average.length);
+			items.unshift("主観評価");
+			var params = {
+ 				aCap: ["新規性", "発言できた", "深い議論", "本音"],
+ 				aMax: 100
+			};
+			rc.draw([items], params);
+		}
+	};
+
+	var showObjectiveEvaluation = function(){
+		document.getElementById("scoreAudio").innerHTML =
+			'盛り上がり: <span class="value">' + score.audio + '</span> 点';
+		document.getElementById("scoreMovie").innerHTML = 
+			'躍動感: <span class="value">' + score.movie + '</span> 点';
+	};
+
+	/*
+	 * save the result
+	 */
+	var saveHistory = function(){
+		var history_item = new HistoryItem({
+			questionnarie: average,
+			score: score
+		});
+		history.add(history_item);
+		Storage.set("history", history);
+	};
+	
+	var showTotalEvaluation = function(){
+		var display = document.getElementById("result");
+		var total_score = score.movie + score.audio;
+		
+		if( total_score > 180){
+    		display.setAttribute("class", "excellent");
+		} else if(total_score > 140){
+			display.setAttribute("class", "great");
+		} else if(total_score > 90){
+			display.setAttribute("class", "good");
+		} else {
+			display.setAttribute("class", "not_bad");
+		}
+	};
+	
+	showSubjectiveEvaluation();
+	showObjectiveEvaluation();
+	showTotalEvaluation();
+	saveHistory();
 });
